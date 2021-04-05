d430e2a8a1f9db85aef225d3fc5e0749344b02e9
Reinhard Scheuerle <reinhard.scheuerle@gmail.com>
2021-04-05 17:30:26 +0200

    fix spelling errors

--- To Do-List/main.js
+++ To Do-List/main.js
@@ -1,12 +1,12 @@
-let addToDoButton = document.getElementByID('addToDo');
-let toDoContainer = document.getElementByID('toDoContainer');
-let inputField = document.getElementByID('inputField');
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click', function(){
    var paragraph = document.createElement('p');
    paragraph.classList.add('paragraph-styling');
    paragraph.innerText = inputField.value;
-   toDoContainer.appendChil(paragraph);
+   toDoContainer.appendChild(paragraph);
    inputfield.value = "";
    paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
